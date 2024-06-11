7d8145c5beabc1e99934e87345acf9a7d9826ae7
Delusoire <deluso7re@outlook.com>
2024-06-11 21:39:14 +0100

    fix: add deno types for lodash

--- deps.js
+++ deps.js
@@ -1,9 +1,10 @@
 /*
  * Copyright (C) 2024 Delusoire
  * SPDX-License-Identifier: GPL-3.0-or-later
- */ // TODO: remove lodash import
+ */ // @deno-types="npm:@types/lodash"
 import ld from "https://esm.sh/lodash";
 export const _ = ld;
+// @deno-types="npm:@types/lodash/fp"
 import ld_fp from "https://esm.sh/lodash/fp";
 export const fp = ld_fp;
 export { default as startCase } from "https://esm.sh/lodash/startCase";
--- deps.ts
+++ deps.ts
@@ -3,10 +3,11 @@
  * SPDX-License-Identifier: GPL-3.0-or-later
  */
 
-// TODO: remove lodash import
+// @deno-types="npm:@types/lodash"
 import ld from "https://esm.sh/lodash";
 export const _ = ld;
 
+// @deno-types="npm:@types/lodash/fp"
 import ld_fp from "https://esm.sh/lodash/fp";
 export const fp = ld_fp;
 
